a481d8f885b69b33e15667acbd67ce789ef6d38b
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-04-01 14:02:07 -0400

    Add files via upload

---
+++ Githubpractice.java
@@ -0,0 +1,11 @@
+package Githubrun;
+
+public class Githubpractice {
+	public static void main(String[]args) {
+		System.out.println("Insert test ");
+		int t = 5;
+		System.out.println(t);
+		
+		}
+
+}

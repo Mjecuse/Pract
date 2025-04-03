4fcd381c962e580f1431db5c8adfdf62d31e81c8
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-04-03 14:58:27 -0400

    Update Githubpractice.java

--- Githubpractice.java
+++ Githubpractice.java
@@ -3,6 +3,7 @@ package Githubrun;
 public class Githubpractice {
 	public static void main(String[]args) {
 		System.out.println("Insert test ");
+		String m = "Moo";
 		int t = 5;
 		int a =6;
 		System.out.println(t);

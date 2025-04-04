1b1f2731f1dc83729dffb448f2f6d367f58eb7a1
BrandonAlz <brandonlopesdeoliveira@outlook.com>
2025-04-03 20:35:52 -0400

    Update Githubpractice.java

--- Githubpractice.java
+++ Githubpractice.java
@@ -3,7 +3,7 @@ package Githubrun;
 public class Githubpractice {
 	public static void main(String[]args) {
 		System.out.println("Insert test ");
-		String m = "Moo";
+		String m = "Moon";
 		int t = 5;
 		int a =6;
 		System.out.println(t);
